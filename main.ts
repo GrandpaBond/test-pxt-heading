994a9a858a68f76fb0d72a7e95997dd7125bd9b6
GrandpaBond <89019204+GrandpaBond@users.noreply.github.com>
2024-06-05 08:07:26 +0100

    lots

--- main.ts
+++ main.ts
@@ -1,22 +1,9 @@
-function face (target: number) {
-    heading2 = heading.degrees()
-    error = angle_between(heading2, target)
-    while (Math.abs(error) > 2) {
-        datalogger.log(
-        datalogger.createCV("target", target),
-        datalogger.createCV("heading", heading2),
-        datalogger.createCV("error", error)
-        )
-        spinRight(error)
-        heading2 = heading.degrees()
-        error = angle_between(heading2, target)
-    }
-}
 function angle_between (first: number, second: number) {
     return (540 + second - first) % 360 - 180
 }
 input.onButtonPressed(Button.A, function () {
-    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, 2 * powerMin)
+    powerMid = (powerMin + powerMax) / 2
+    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, powerMid)
     heading.scanClockwise(2000)
     Kitronik_Move_Motor.stop()
     basic.showString("Go?")
@@ -26,6 +13,42 @@ input.onButtonPressed(Button.A, function () {
         basic.showArrow(ArrowNames.East)
     }
 })
+function finalFace (target: number) {
+    heading2 = heading.degrees()
+    error = angle_between(heading2, target)
+    while (Math.abs(error) > 1) {
+        spinRight(error)
+        Kitronik_Move_Motor.stop()
+        heading2 = heading.degrees()
+        error = angle_between(heading2, target)
+        datalogger.log(
+        datalogger.createCV("target", target),
+        datalogger.createCV("heading", heading2),
+        datalogger.createCV("error", error)
+        )
+    }
+}
+function initialFace (target: number) {
+    heading2 = heading.degrees()
+    error = angle_between(heading2, target)
+    datalogger.log(
+    datalogger.createCV("target", target),
+    datalogger.createCV("heading", heading2),
+    datalogger.createCV("error", error)
+    )
+    if (error > 0) {
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, powerMid)
+    } else {
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, powerMid)
+    }
+    basic.pause(Math.abs(powerMid * degreeTime) + inertia)
+    Kitronik_Move_Motor.stop()
+    datalogger.log(
+    datalogger.createCV("target", target),
+    datalogger.createCV("heading", heading2),
+    datalogger.createCV("error", error)
+    )
+}
 input.onButtonPressed(Button.B, function () {
     result = heading.setNorth()
     basic.showNumber(result)
@@ -34,8 +57,15 @@ input.onButtonPressed(Button.B, function () {
     } else if (result == -2) {
         basic.showString("Weak field!")
     } else {
+        degreeTime = heading.spinTime() / 360
         for (let index = 0; index <= 4; index++) {
-            star(index + 3, 500)
+            initialFace(index * 90)
+            finalFace(index * 90)
+            basic.pause(2000)
+        }
+        basic.pause(5000)
+        for (let index = 0; index <= 3; index++) {
+            star(index * 2 + 3, 500)
             basic.pause(2000)
         }
     }
@@ -47,32 +77,45 @@ function spinRight (step: number) {
         Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, Math.min(powerFactor * step, 0 - powerMin))
     }
     basic.pause(Math.min(timeFactor * Math.abs(step), timeMin))
-    Kitronik_Move_Motor.stop()
 }
 function star (points: number, leg: number) {
     turn = 180 - 180 / points
     target = heading.degrees()
     for (let index = 0; index < points; index++) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 3 * powerMin)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, powerMid)
         basic.pause(leg)
         Kitronik_Move_Motor.stop()
         target = (360 + (target + turn)) % 360
-        face(target)
+        initialFace(target)
+        finalFace(target)
     }
 }
 let target = 0
 let turn = 0
 let result = 0
+let degreeTime = 0
 let error = 0
 let heading2 = 0
+let powerMid = 0
+let inertia = 0
 let timeMin = 0
+let powerMax = 0
 let powerMin = 0
 let timeFactor = 0
 let powerFactor = 0
+datalogger.deleteLog()
+datalogger.includeTimestamp(FlashLogTimeStampFormat.Milliseconds)
+datalogger.setColumnTitles(
+"target",
+"heading",
+"error"
+)
 powerFactor = 5
 timeFactor = 3
 powerMin = 30
+powerMax = 100
 timeMin = 50
+inertia = 100
 basic.showString("Scan")
 for (let index = 0; index < 3; index++) {
     basic.clearScreen()
